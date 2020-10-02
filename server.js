64e44647bb869e2c054552ea97a5583be19a39a3
tbastuk <tbasturk@uncc.edu>
2020-10-02 12:36:04 -0400

    Loading data from JSON file Updated

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-let budget = require('/Users/talha/dev/week03/personal-budget/server.json');
-
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    
+    res.sendFile("server.json",{root:__dirname});
 });
 
 app.listen(port, () => {
